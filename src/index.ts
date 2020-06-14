502fe7998d3167121d7b45f0459b748324a6c2b6
qazx7412 <git@newgenerations.im>
2020-06-14 18:42:36 +0900

    collision detection

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,17 @@ let ctx = <CanvasRenderingContext2D>canvas.getContext("2d")
 let x = canvas.width / 2
 let y = canvas.height - 30
 
-const dx = 2
-const dy = -2
+let dx = 2
+let dy = -2
+const ballRadius = 10
 
 const drawBall = () => {
   ctx.beginPath()
-  ctx.arc(x, y, 10, 0, Math.PI * 2)
+
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2)
   ctx.fillStyle = "#0095DD"
   ctx.fill()
+
   ctx.closePath()
 }
 
@@ -19,6 +22,13 @@ const draw = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
 
   drawBall()
+
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx
+  }
+  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    dy = -dy
+  }
 
   x += dx
   y += dy
