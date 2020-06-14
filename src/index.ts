c91cffc1e33d012d989ae86fee6e8519430e6d1b
qazx7412 <git@newgenerations.im>
2020-06-14 18:24:10 +0900

    move ball

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,31 @@
+const canvas = <HTMLCanvasElement>document.getElementById("myCanvas")
+let ctx = <CanvasRenderingContext2D>canvas.getContext("2d")
+
+let x = canvas.width / 2
+let y = canvas.height - 30
+
+const dx = 2
+const dy = -2
+
+const drawBall = () => {
+  ctx.beginPath()
+  ctx.arc(x, y, 10, 0, Math.PI * 2)
+  ctx.fillStyle = "#0095DD"
+  ctx.fill()
+  ctx.closePath()
+}
+
+const draw = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+  drawBall()
+
+  x += dx
+  y += dy
+}
 
 const main = () => {
-  const canvas = <HTMLCanvasElement>document.getElementById("myCanvas")
-  let ctx = <CanvasRenderingContext2D>canvas.getContext("2d")
-
-  ctx.beginPath()
-  ctx.rect(20, 40, 50, 50)
-  ctx.fillStyle = "#FF0000"
-  ctx.fill()
-  ctx.closePath()
-
-  ctx.beginPath()
-  ctx.arc(240, 160, 20, 0, Math.PI * 2, false)
-  ctx.fillStyle = "green"
-  ctx.fill()
-  ctx.closePath()
-
-  ctx.beginPath()
-  ctx.rect(160, 10, 100, 40)
-  ctx.strokeStyle = "rgba(0, 0, 255, 0.5)"
-  ctx.stroke()
-  ctx.closePath()
+  setInterval(draw, 10)
 }
 
 main()
