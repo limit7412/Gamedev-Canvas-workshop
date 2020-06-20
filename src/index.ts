719ae7d7ccc7e04f9e5761ab26ef49e0cd9ce77e
qazx7412 <git@newgenerations.im>
2020-06-20 21:45:17 +0900

    add paddle

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,13 @@ let dx = 2
 let dy = -2
 const ballRadius = 10
 
+const paddleHeight = 10
+const paddleWidth = 75
+let paddleX = (canvas.width - paddleWidth) / 2
+
+let rightPressed = false
+let leftPressed = false
+
 const drawBall = () => {
   ctx.beginPath()
 
@@ -18,10 +25,21 @@ const drawBall = () => {
   ctx.closePath()
 }
 
+const drawPaddle = () => {
+  ctx.beginPath()
+
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight)
+  ctx.fillStyle = "#0095DD"
+  ctx.fill()
+
+  ctx.closePath()
+}
+
 const draw = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
 
   drawBall()
+  drawPaddle()
 
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     dx = -dx
@@ -32,9 +50,37 @@ const draw = () => {
 
   x += dx
   y += dy
+
+  if (rightPressed && paddleX < canvas.width - paddleWidth) {
+    paddleX += 7;
+  }
+  else if (leftPressed && paddleX > 0) {
+    paddleX -= 7;
+  }
+}
+
+const keyDownHandler = (e: KeyboardEvent) => {
+  if (e.key == "Right" || e.key == "ArrowRight") {
+    rightPressed = true
+  }
+  else if (e.key == "Left" || e.key == "ArrowLeft") {
+    leftPressed = true
+  }
+}
+
+const keyUpHandler = (e: KeyboardEvent) => {
+  if (e.key == "Right" || e.key == "ArrowRight") {
+    rightPressed = false
+  }
+  else if (e.key == "Left" || e.key == "ArrowLeft") {
+    leftPressed = false
+  }
 }
 
 const main = () => {
+  document.addEventListener("keydown", keyDownHandler, false)
+  document.addEventListener("keyup", keyUpHandler, false)
+
   setInterval(draw, 10)
 }
 
