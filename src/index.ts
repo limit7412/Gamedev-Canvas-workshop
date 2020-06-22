02818170a3ec6ff83b1c328198696e55c6f33271
qazx7412 <git@newgenerations.im>
2020-06-22 22:10:46 +0900

    gameover

--- src/index.ts
+++ src/index.ts
@@ -44,10 +44,17 @@ const draw = () => {
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     dx = -dx
   }
-  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+  if (y + dy < ballRadius) {
     dy = -dy
+  } else if (y + dy > canvas.height - ballRadius) {
+    if (x > paddleX && x < paddleX + paddleWidth) {
+      dy = -dy
+    }
+    else {
+      alert("GAME OVER");
+      document.location.reload();
+    }
   }
-
   x += dx
   y += dy
 
@@ -77,11 +84,11 @@ const keyUpHandler = (e: KeyboardEvent) => {
   }
 }
 
-const main = () => {
-  document.addEventListener("keydown", keyDownHandler, false)
-  document.addEventListener("keyup", keyUpHandler, false)
+// const main = () => {
+document.addEventListener("keydown", keyDownHandler, false)
+document.addEventListener("keyup", keyUpHandler, false)
 
-  setInterval(draw, 10)
-}
+let interval = setInterval(draw, 10)
+// }
 
-main()
+// main()
